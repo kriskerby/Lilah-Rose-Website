5ada8cbb2b892f07dfa1282dbb5f9d7e068fff81
kriskerby <kriskerby@gmail.com>
2021-05-11 19:31:34 -0700

    messing with alignments

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
+    
 
+  const image = document.querySelector(".image");
 
-
-
+  async function imageLoad () {
+      const res = fetch("LilahRose COLOR1.JPG")
+      console.log(res);
+  }
+imageLoad();
 });
